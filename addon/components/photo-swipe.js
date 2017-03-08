b3e904a8d2626be10d4b3e576b9897fb8a43cdfe
Stanislav Romanov <kaermorchen@gmail.com>
2017-03-08 10:20:13 +0300

    Added destroy event

--- addon/components/photo-swipe.js
+++ addon/components/photo-swipe.js
@@ -4,14 +4,34 @@
 import Ember from 'ember';
 import layout from '../templates/components/photo-swipe';
 
+const { on, isPresent, isEmpty } = Ember;
+
 export default Ember.Component.extend({
   layout,
 
+  pswp: null,
+
+  destroyPswp: on('willDestroyElement', function() {
+    const pswp = this.get('pswp');
+
+    if (isPresent(pswp)) {
+      pswp.destroy();
+      this.set('pswp', null);
+    }
+  }),
+
   actions: {
     open(items, options) {
-      const pswpElement = this.$('.pswp')[0];
+      if (isEmpty(items)) {
+        return;
+      }
 
-      new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options).init();
+      const pswpElement = this.$('.pswp')[0];
+      const pswp = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+
+      pswp.init();
+
+      this.set('pswp', pswp);
     }
   }
 });

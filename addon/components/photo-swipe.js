0b95593c1310dfbf5a48e36e586b75ac97e38f81
Stanislav Romanov <kaermorchen@gmail.com>
2017-03-08 12:19:13 +0300

    Added pswpOptions

--- addon/components/photo-swipe.js
+++ addon/components/photo-swipe.js
@@ -4,30 +4,75 @@
 import Ember from 'ember';
 import layout from '../templates/components/photo-swipe';
 
-const { on, isPresent, isEmpty } = Ember;
+const { assign, computed, on, isPresent, isEmpty } = Ember;
 
 export default Ember.Component.extend({
   layout,
 
+  concatenatedProperties: ['pswpOptions'],
+
+  pswpOptions: [
+    'index',
+    'getThumbBoundsFn',
+    'showHideOpacity',
+    'showAnimationDuration',
+    'hideAnimationDuration',
+    'bgOpacity',
+    'spacing',
+    'allowPanToNext',
+    'maxSpreadZoom',
+    'getDoubleTapZoom',
+    'loop',
+    'pinchToClose',
+    'closeOnScroll',
+    'closeOnVerticalDrag',
+    'mouseUsed',
+    'escKey',
+    'arrowKeys',
+    'history',
+    'galleryUID',
+    'galleryPIDs',
+    'errorMsg',
+    'preload',
+    'mainClass',
+    'getNumItemsFn',
+    'focus',
+    'isClickableElement',
+    'modal',
+  ],
   pswp: null,
+
+  options: computed(function() {
+    const pswpOptions = this.get('pswpOptions');
+    const options = {};
+
+    pswpOptions.forEach(optionName => {
+      if (this.get(optionName) !== undefined) {
+        options[optionName] = this.get(optionName);
+      }
+    });
+
+    return options;
+  }),
 
   destroyPswp: on('willDestroyElement', function() {
     const pswp = this.get('pswp');
 
     if (isPresent(pswp)) {
-      pswp.destroy();
-      this.set('pswp', null);
+      pswp.close();
     }
   }),
 
   actions: {
-    open(items, options) {
+    open(items, actionOptions) {
       if (isEmpty(items)) {
         return;
       }
 
       const pswpElement = this.$('.pswp')[0];
-      const pswp = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+      const options = this.get('options');
+      const assignedOptions = assign({}, options, actionOptions);
+      const pswp = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, assignedOptions);
 
       pswp.init();
 
--- tests/integration/components/photo-swipe-test.js
+++ tests/integration/components/photo-swipe-test.js
@@ -5,7 +5,7 @@ moduleForComponent('photo-swipe', 'Integration | Component | photo swipe', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
@@ -22,4 +22,22 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('pswpOptions is passed', function (assert) {
+  this.set('pswp', null);
+  this.set('items', [{
+    src: 'https://unsplash.it/1024/768/?random&image1',
+    w: 1024,
+    h: 768,
+  }]);
+
+  this.render(hbs`
+    {{#photo-swipe history=false pswp=pswp as |photoSwipe|}}
+      <button {{action 'open' items target=photoSwipe}}>Aaaaaaaaaaaaand Open</button>
+    {{/photo-swipe}}
+  `);
+  this.$('button').click();
+
+  assert.equal(this.get('pswp.options.history'), false);
 });

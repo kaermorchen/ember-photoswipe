fd13bdf9428b8116566b2c86e3b40c9e32767913
Stanislav Romanov <kaermorchen@gmail.com>
2017-03-13 14:23:53 +0300

    Fixed demo-preview url

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    // Add options here
+    fingerprint: {
+      enabled: true,
+      exclude: [
+        'assets/images/demo-preview.png'
+      ]
+    }
   });
 
   /*
